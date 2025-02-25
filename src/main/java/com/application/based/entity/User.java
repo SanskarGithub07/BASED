667a7c89ca4d798c83f25f798ae7b431b871d1fb
SanskarGithub07 <117458101+SanskarGithub07@users.noreply.github.com>
2025-02-25 22:14:24 +0530

    Write hello world program and database test.

---
+++ src/main/java/com/application/based/entity/User.java
@@ -0,0 +1,35 @@
+package com.application.based.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+public class User {
+    @Id
+    @SequenceGenerator(name = "user_sequence", sequenceName = "user_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_sequence")
+    private Long userId;
+
+    @NotBlank(message = "Please add username")
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+    @NotEmpty(message = "Email cannot be empty")
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(length = 60)
+    private String password;
+    private String profilePicture;
+    private String status;
+}
