56f2efe841c0b8fa84d7a09cdf22595e5c247b3a
SanskarGithub07 <117458101+SanskarGithub07@users.noreply.github.com>
2025-04-05 19:39:49 +0530

    Add partially complete frontend and backend for UC-003.

--- src/main/java/com/application/based/service/BookServiceImpl.java
+++ src/main/java/com/application/based/service/BookServiceImpl.java
@@ -6,6 +6,8 @@ import com.application.based.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BookServiceImpl implements BookService{
     @Autowired
@@ -13,16 +15,42 @@ public class BookServiceImpl implements BookService{
 
     @Override
     public Book addBook(BookModel bookModel) {
-        System.out.println(bookModel.getBookName());
+        System.out.println(bookModel.getAuthorName());
         Book book = Book.builder()
                         .isbn(bookModel.getIsbn())
-                                .author(bookModel.getAuthorName())
-                                        .bookname(bookModel.getBookName())
+                                .authorName(bookModel.getAuthorName())
+                                        .bookName(bookModel.getBookName())
                                                 .publicationYear(bookModel.getPublicationYear())
                                                         .quantity(bookModel.getQuantity())
                                                                 .price(bookModel.getPrice())
 
                 .build();
         return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> searchBooks(String authorName, Long isbnNumber, String bookName){
+        if(isbnNumber != null){
+            Book book = bookRepository.findByIsbn(isbnNumber);
+            if(book != null){
+                return List.of(book);
+            }
+            else{
+                return List.of();
+            }
+        }
+
+        if(authorName != null && bookName != null){
+            return bookRepository.findByAuthorNameContainingIgnoreCaseAndBookNameContainingIgnoreCase(authorName, bookName);
+        }
+        else if(authorName != null){
+            return bookRepository.findByAuthorNameContainingIgnoreCase(authorName);
+        }
+        else if(bookName != null){
+            return bookRepository.findByBookNameContainingIgnoreCase(bookName);
+        }
+        else{
+            return List.of();
+        }
     }
 }
