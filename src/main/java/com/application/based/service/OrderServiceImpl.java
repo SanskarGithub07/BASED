681b4346a52af69be7830078c824fd0e69d63af6
SanskarGithub07 <117458101+SanskarGithub07@users.noreply.github.com>
2025-04-24 22:32:49 +0530

    Add download receipt backend and frontend.

--- src/main/java/com/application/based/service/OrderServiceImpl.java
+++ src/main/java/com/application/based/service/OrderServiceImpl.java
@@ -21,6 +21,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -136,5 +137,10 @@ public class OrderServiceImpl implements OrderService {
         List<Order> orders = orderRepository.findAllByUserOrderByCreatedDateDesc(user);
         log.trace("Found {} orders for user {}", orders.size(), user.getUsername());
         return orders;
+    }
+
+    @Override
+    public Optional<Order> findOrderById(Long orderId){
+        return orderRepository.findById(orderId);
     }
 }
